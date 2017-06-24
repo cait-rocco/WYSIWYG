1de91fc4c50245189b5475f063eae88ea3a69717
Cait Rocco <cait.rocco@gmail.com>
2017-06-24 12:56:25 -0500

    initial creation of files

---
+++ wysiwyg.js
@@ -0,0 +1,32 @@
+var famPeople = [
+	{
+		title: "Presidentail Candidate",
+		name: "Hillary Clinton",
+		bio: "An American politician who was the 67th United States Secretary of State from 2009 to 2013, U.S. Senator from New York from 2001 to 2009, First Lady of the United States from 1993 to 2001, and the Democratic Party's nominee for President of the United States in the 2016 election.",
+		image: "https://http://images.huffingtonpost.com/2016-10-10-1476135120-8225852-Hillary1.jpg",
+		lifespan: {
+			birth: 1947,
+			death: "----"
+		}
+	},
+	{
+		title: "Senator",
+		name: "Elizabeth Warren",
+		bio: "An American academic and politician. She is a member of the Democratic Party and is the senior United States Senator from Massachusetts. Warren was formerly a professor of law, and taught at the University of Texas School of Law, the University of Pennsylvania Law School, and most recently at Harvard Law School. A prominent scholar specializing in bankruptcy law, Warren was among the most cited in the field of commercial law before starting her political career.",
+		image: "https://static.elizabethwarren.com/assets/images/intro-video-still.jpg",
+		lifespan: {
+			birth: 1949,
+			death: "----"
+		}	
+	},
+	{
+		title: "Supreme Court Justice",
+		name: "Ruth Bader Ginsburg",
+		bio: "An Associate Justice of the Supreme Court of the United States. Ginsburg was appointed by President Bill Clinton and took the oath of office on August 10, 1993. She is the second female justice to be confirmed to the Court (after Sandra Day O'Connor), and one of four female justices to be confirmed (with Sonia Sotomayor and Elena Kagan, who are still serving). ",
+		image: "https://en.wikipedia.org/wiki/Ruth_Bader_Ginsburg#/media/File:Ruth_Bader_Ginsburg_2016_portrait.jpg",
+		lifespan: {
+			birth: 1933,
+			death: "----"
+		}
+	}
+]
